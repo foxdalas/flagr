abbc06720a802c65e8fd0d36664a270f28f7a7cd
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2025-09-03 19:17:48 +0700

    Extend EvalCache export with a querying option

--- pkg/handler/eval_cache_test.go
+++ pkg/handler/eval_cache_test.go
@@ -1,12 +1,14 @@
 package handler
 
 import (
+	"slices"
 	"testing"
 
-	"github.com/newrelic/go-agent/v3/newrelic"
 	"github.com/foxdalas/flagr/pkg/config"
 	"github.com/foxdalas/flagr/pkg/entity"
 	"github.com/foxdalas/flagr/swagger_gen/models"
+	"github.com/foxdalas/flagr/swagger_gen/restapi/operations/export"
+	"github.com/newrelic/go-agent/v3/newrelic"
 
 	"github.com/prashantv/gostub"
 	"github.com/stretchr/testify/assert"
@@ -107,4 +109,109 @@ func TestReloadMapCacheWithNewRelic(t *testing.T) {
 		err := ec.reloadMapCache()
 		assert.NoError(t, err)
 	})
+}
+
+func TestEvalCacheExport(t *testing.T) {
+	ec := GenFixtureEvalCacheWithFlags([]entity.Flag{
+		entity.GenFixtureFlagWithTags(1, "first", true, []string{"tag1", "tag2"}),
+		entity.GenFixtureFlagWithTags(2, "second", true, []string{"tag2", "tag3"}),
+		entity.GenFixtureFlagWithTags(3, "third", false, []string{"tag2", "tag3"}),
+		entity.GenFixtureFlagWithTags(4, "fourth", true, []string{}),
+	})
+
+	t.Run("should be able to query cache via flag ids", func(t *testing.T) {
+		exportedFlags := ec.export(export.GetExportEvalCacheJSONParams{Ids: []int64{1, 3}}).Flags
+		assert.Len(t, exportedFlags, 2)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(1)))
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(1)))
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(3)))
+	})
+
+	t.Run("should be able to query cache via flag keys", func(t *testing.T) {
+		exportedFlags := ec.export(export.GetExportEvalCacheJSONParams{Keys: []string{"second", "fourth"}}).Flags
+		assert.Len(t, exportedFlags, 2)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(2)))
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(4)))
+	})
+
+	t.Run("should be able to query cache via enabled property", func(t *testing.T) {
+		tru := true
+		exportedFlags := ec.export(export.GetExportEvalCacheJSONParams{Enabled: &tru}).Flags
+		assert.Len(t, exportedFlags, 3)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(1)))
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(2)))
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(4)))
+
+		fals := false
+		exportedFlags = ec.export(export.GetExportEvalCacheJSONParams{Enabled: &fals}).Flags
+		assert.Len(t, exportedFlags, 1)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(3)))
+	})
+
+	t.Run("should be able to query cache via tags with default ANY semantics", func(t *testing.T) {
+		exportedFlags := ec.export(export.GetExportEvalCacheJSONParams{Tags: []string{"tag1", "tag2"}}).Flags
+		assert.Len(t, exportedFlags, 3)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(1)))
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(2)))
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(3)))
+
+		fals := false
+		exportedFlags = ec.export(export.GetExportEvalCacheJSONParams{All: &fals, Tags: []string{"tag1", "tag2"}}).Flags
+		assert.Len(t, exportedFlags, 3)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(1)))
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(2)))
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(3)))
+	})
+
+	t.Run("should be able to query cache via tags with ALL semantics", func(t *testing.T) {
+		tru := true
+		exportedFlags := ec.export(export.GetExportEvalCacheJSONParams{All: &tru, Tags: []string{"tag1", "tag2"}}).Flags
+		assert.Len(t, exportedFlags, 1)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(1)))
+	})
+
+	t.Run("flag ids query should have precedence over other queries", func(t *testing.T) {
+		exportedFlags := ec.export(export.GetExportEvalCacheJSONParams{Ids: []int64{4}, Keys: []string{"first", "second"}}).Flags
+		assert.Len(t, exportedFlags, 1)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(4)))
+
+		fals := false
+		exportedFlags = ec.export(export.GetExportEvalCacheJSONParams{Ids: []int64{4}, Enabled: &fals}).Flags
+		assert.Len(t, exportedFlags, 1)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(4)))
+
+		exportedFlags = ec.export(export.GetExportEvalCacheJSONParams{Ids: []int64{4}, Tags: []string{"tag1"}}).Flags
+		assert.Len(t, exportedFlags, 1)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(4)))
+	})
+
+	t.Run("flag keys query should have precedence over enabled and tags queries", func(t *testing.T) {
+		fals := false
+		exportedFlags := ec.export(export.GetExportEvalCacheJSONParams{Keys: []string{"fourth"}, Enabled: &fals}).Flags
+		assert.Len(t, exportedFlags, 1)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(4)))
+
+		exportedFlags = ec.export(export.GetExportEvalCacheJSONParams{Keys: []string{"fourth"}, Tags: []string{"tag1"}}).Flags
+		assert.Len(t, exportedFlags, 1)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(4)))
+	})
+
+	t.Run("should be able to combine enabled and tags queries", func(t *testing.T) {
+		tru := true
+		exportedFlags := ec.export(export.GetExportEvalCacheJSONParams{Enabled: &tru, Tags: []string{"tag2"}}).Flags
+		assert.Len(t, exportedFlags, 2)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(1)))
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(2)))
+
+		fals := false
+		exportedFlags = ec.export(export.GetExportEvalCacheJSONParams{Enabled: &fals, Tags: []string{"tag2"}}).Flags
+		assert.Len(t, exportedFlags, 1)
+		assert.True(t, slices.ContainsFunc(exportedFlags, withID(3)))
+	})
+}
+
+func withID(id uint) func(entity.Flag) bool {
+	return func(f entity.Flag) bool {
+		return f.ID == id
+	}
 }
